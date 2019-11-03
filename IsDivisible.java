e609ee406f8a280bdb150d14ee9ed532744e6b6e
Baasbank <baasb4real@yahoo.com>
2019-11-03 09:52:32 +0100

    check whether one integer is divisible by another

---
+++ IsDivisible.java
@@ -0,0 +1,14 @@
+/*
+ * Write a method named isDivisible that takes two integers, n and m, and that returns true if n is divisible by m, and false otherwise.
+ */
+
+public class IsDivisible {
+  public static boolean isDivisible (int n, int m) {
+    return n % m == 0;
+  }
+/* Remove this multiline comment to test the method
+  public static void main (String[] args) {
+    System.out.println(isDivisible(6,4));
+  }
+  */
+}
